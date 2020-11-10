148cc9b2b6e87ffea56fd1f485cd439662b0d28b
nnobinelo <nnobinelo98@live.com>
2020-11-10 09:49:14 -0500

    added comments to code in /board_communication

--- board_communication/util.py
+++ board_communication/util.py
@@ -1,10 +1,32 @@
+import subprocess
+
+
 def bytes_ip_to_ip_str(btyes_str, byte_order='little', signed=False, ipv4=True):
+    """
+    Converts a string of bytes, assumed to be an IP address from the , to an IP string w/ the bytes of the
+    IP separated by dots (xxx.xxx.xxx.xxx). Currently only implemented for IPv4 addresses
+
+    :param btyes_str: (str) string of bytes in the form: "b'\xe8\x89\xa8\xc0'"
+    :param byte_order: (str) byte order the bytes string should be decoded from passed to python's int.from_bytes() func
+    :param signed: (bool) specifies if the bytes string in signed or not and pass to int.from_bytes()
+    :param ipv4: (bool) indicates if the IP should be decoded as an IPv4 or IPv6 address
+    :return: (str) the IP string w/ the bytes of the IP separated by dots (xxx.xxx.xxx.xxx)
+    """
+
     ip_integer = int.from_bytes(btyes_str, byteorder=byte_order, signed=signed)
     ip_str = int_ip_to_ip_str(ip_integer, ipv4=ipv4)
     return ip_str
 
 
 def int_ip_to_ip_str(ip_integer, ipv4=True):
+    """
+    Converts an IP in the form of an integer, from the CC3220SF MCU, to an IP string. Currently only
+    implemented for IPv4 addresses
+
+    :param ip_integer: (int) integer represeting an IP from the
+    :param ipv4: (bool) indicates if the IP should be decoded as an IPv4 or IPv6 address
+    :return: (str) the IP string w/ the bytes of the IP separated by dots (xxx.xxx.xxx.xxx)
+    """
     ip_byte_strings = []
     if ipv4:
         for i in range(3, -1, -1):
@@ -17,6 +39,14 @@ def int_ip_to_ip_str(ip_integer, ipv4=True):
 
 
 def hex_ip_to_ip_str(hex_str, ipv4=True):
+    """
+    Converts a hex string representing an IP address, from the CC3220SF MCU, to an IP string. Currently only
+    implemented for IPv4 addresses
+
+    :param hex_str: (str) hex string representing IP in the form: "c0a889df"
+    :param ipv4: (bool) indicates if the IP should be decoded as an IPv4 or IPv6 address
+    :return: (str) the IP string w/ the bytes of the IP separated by dots (xxx.xxx.xxx.xxx)
+    """
     if ipv4:
         if len(hex_str) != 8:
             print(f'hex string has a length greater than 8: len={len(hex_str)}')
@@ -30,6 +60,27 @@ def hex_ip_to_ip_str(hex_str, ipv4=True):
         return ip_str
     else:
         return None
+
+
+def run_cmd(cmd_str, show=True):
+    """
+    Runs the shell command specified in cmd_str w/ subprocess.run and will raise an error and print stacktrace on error
+    of the command, otherwise prints the stdout of the command
+
+    :param cmd_str: (str) shell command
+    :param show: (bool) when true prints the stdout of the shell command on success, otherwise prints nothing
+    :return: (str) the stdout of the shell command on success as a unicode string
+    """
+
+    proc = subprocess.run(cmd_str.split(), capture_output=True, encoding="utf-8")
+    if proc.returncode != 0:
+        print(f'*** ERROR occurred during subprocess.run call, command: ***\n{" ".join(proc.args)}'
+              f'\n*** stderr: ***\n{proc.stdout.strip()}')
+        raise RuntimeError
+    else:
+        if show:
+            print(proc.stdout.strip())
+    return proc.stdout.strip()
 
 
 if __name__ == "__main__":
--- board_communication/windows_ap.py
+++ board_communication/windows_ap.py
@@ -1,25 +1,51 @@
 import subprocess
 import re
+from util import run_cmd
 
 
 class WindowsSoftAP:
     def __init__(self, ssid="test_ap_1", key="12345678"):
+        """
+        Class to create and start the netsh "hostednetwork" soft AP Windows feature
+
+        :param ssid: (str) ssid for AP
+        :param key: (str) password for AP
+        """
+
         self.ssid = ssid
         self.key = key
         self.ipv4 = None
 
     def start_ap(self):
-        print("\n*** CONFIGURING WINDOWS HOSTEDNETWORK ***\n")
-        run_cmd(f"netsh wlan set hostednetwork mode=allow ssid={self.ssid} key={self.key}")
+        """
+        Starts the AP and sets self.ipv4 to the IPv4 of the AP
+        """
 
-        print("\n*** STARTING WINDOWS HOSTEDNETWORK ***\n")
-        run_cmd("netsh wlan start hostednetwork")
-
-        print("\n*** RETRIEVING IP OF WINDOWS HOSTEDNETWORK ***\n")
         self.ipv4 = self._get_ipv4()
-        print(f"*** WINDOWS HOSTEDNETWORK IP: {self.ipv4} ***")
+
+        if not self.ipv4:
+            print("\n*** CONFIGURING WINDOWS HOSTEDNETWORK ***\n")
+            run_cmd(f"netsh wlan set hostednetwork mode=allow ssid={self.ssid} key={self.key}")
+
+            print("\n*** STARTING WINDOWS HOSTEDNETWORK ***\n")
+            run_cmd("netsh wlan start hostednetwork")
+
+            print("\n*** RETRIEVING IP OF WINDOWS HOSTEDNETWORK ***\n")
+            self.ipv4 = self._get_ipv4()
+
+        if not self.ipv4:
+            print("*** ERROR: UNABLE TO RETRIEVE WINDOWS HOSTNETWORK IP, CHECK IF RUNNING ***")
+            return
+
+        print(f"*** WINDOWS HOSTEDNETWORK STARTED, IP: {self.ipv4} ***")
 
     def get_ipv4(self):
+        """
+        Returns the IPv4 address of the AP
+
+        :return: (str) IPv4 address of the AP in the form: "xxx.xxx.xxx.xxx"
+        """
+
         if not self.ipv4:
             self.ipv4 = self._get_ipv4()
             if not self.ipv4:
@@ -30,15 +56,26 @@ class WindowsSoftAP:
             return self.ipv4
 
     def stop_ap(self):
+        """
+        Stops the AP
+        """
+
         print("\n*** STOPPING WINDOWS HOSTEDNETWORK ***\n")
         run_cmd("netsh wlan stop hostednetwork")
+        print("\n*** WINDOWS HOSTEDNETWORK STOPPED ***")
 
     def _get_ipv4(self):
+        """
+        Retrieves and returns the IPv4 address of the hostednetwork AP from an "ipconfig /all" call
+
+        :return: (str) IPv4 address of the AP in the form: "xxx.xxx.xxx.xxx"
+        """
         stdout = run_cmd("ipconfig /all", show=False)
         record_next_ip = False
         ip = None
         for line in stdout.lower().split(sep='\n'):
             if not record_next_ip:
+                # this will be read in the "description" line of an adapter
                 if "microsoft hosted network virtual adapter" in line:
                     record_next_ip = True
             else:
@@ -46,22 +83,15 @@ class WindowsSoftAP:
                     match = re.search(r'(\d+)\.(\d+)\.(\d+)\.(\d+)', line)
                     ip = match.group(0)
                     break
+                # if another "description" is read before an "ipv4 address" there is no associated IPv4 address with
+                # the "microsoft hosted network virtual adapter" adapter, most likely because the hostednetwork
+                # is not running
+                elif "description" in line:
+                    break
         return ip
-
-
-def run_cmd(cmd_str, show=True):
-    proc = subprocess.run(cmd_str.split(), capture_output=True, encoding="utf-8")
-    if proc.returncode != 0:
-        print(f'*** ERROR occurred during subprocess.run call, command: ***\n{" ".join(proc.args)}'
-              f'\n*** stderr: ***\n{proc.stdout.strip()}')
-        raise RuntimeError
-    else:
-        if show:
-            print(proc.stdout.strip())
-    return proc.stdout.strip()
 
 
 if __name__ == "__main__":
     ap = WindowsSoftAP()
     ap.start_ap()
-    ap.stop_ap()
+    # ap.stop_ap()
