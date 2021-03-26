3c6ae0775f36fc7562d6eb01808d74f535400e7f
dgarland11 <72989074+dgarland11@users.noreply.github.com>
2021-03-25 22:55:30 -0400

    plotting functions

---
+++ board_communication/system_integration.py
@@ -0,0 +1,223 @@
+import socket
+import time
+import os
+from PyAccessPoint import pyaccesspoint
+import datetime
+import logging
+import math
+from board_communication.parse_and_plot import plot_tcp_data
+
+WINDOWS = True
+ENTRY_PORT = 10000
+
+#whether or not to save experiment
+SAVE = False
+
+ROOT_FOLDER = ""
+
+def str_date():
+    # returns the date as a string formatted as <year>-<month>-<day> hours:minutes:seconds
+    a = str(datetime.datetime.now())
+    return a
+
+
+def setup():
+    global ROOT_FOLDER, logger
+
+    # if data folder does not exist, create it
+    data_folder = os.path.join(os.getcwd(), "timestamp_data")
+    if not os.path.exists(data_folder):
+        os.mkdir(data_folder)
+
+    # create experiment folder
+    date = str_date()
+    formatted_date = str(date).replace(" ", "_").split(".")[0].replace(":", ";")
+    ROOT_FOLDER = os.path.join(os.getcwd(), "timestamp_data", formatted_date)
+    os.mkdir(ROOT_FOLDER)
+
+    # setup logging
+    logger = logging.getLogger("experiment_log")
+    logger.setLevel(logging.INFO)
+    logfile = os.path.join(ROOT_FOLDER, "laptop.log")
+    logging.basicConfig(filename=logfile, format='%(message)s')
+    logging.getLogger("experiment_log").addHandler(logging.StreamHandler())
+
+
+def create_files(uid):
+    global ROOT_FOLDER
+    # uid: client IP address
+    ip_last3 = uid.split(".")[-1]
+    filename = ip_last3 + ".txt"
+    board_file = os.path.join(os.getcwd(), "timestamp_data", ROOT_FOLDER, filename)
+    if not os.path.exists(board_file):
+        f = open(board_file, 'a')
+        f.write("beacon_timestamp,local_timestamp")
+        f.close()
+    return board_file
+
+
+def setup_ap():
+    access_point = pyaccesspoint.AccessPoint()
+    access_point.start()
+
+    if access_point.is_running():
+        logger.info("Hostapd is running")
+    else:
+        logger.info("Hostapd failed to start")
+        exit(-1)
+    return access_point
+
+
+def linux(plot = True):
+    global ENTRY_PORT
+
+    readings = {"wrist":{}, "base":{}}
+
+    try:
+        ap = setup_ap()
+        server_ip = ap.ip
+        ENTRY_PORT = 10000
+
+        entry_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
+        entry_address = (server_ip, ENTRY_PORT)
+        entry_socket.bind(entry_address)  # Bind the socket to the port
+        entry_socket.listen(2)  # Listen for incoming connections
+        last_time = time.time()
+
+
+        for i in range(2):
+
+            logger.info('waiting for a connection')
+            connection, client_address = entry_socket.accept()
+
+            logger.info(f'*** connection from {client_address} ***')
+            data = connection.recv(100000)
+            for i in range(10):
+                data += connection.recv(10000000)
+
+            logger.info(data)
+            if plot:
+                name, data = assemble_data_for_plot(data)
+                readings[name] = data
+
+            else:
+                parse_mcu_msg(data, client_address)
+    except Exception as e:
+        logger.info(e)
+
+    plot_tcp_data(readings["wrist"], readings["base"])
+
+
+
+def assemble_data_for_plot(data):
+    # from wrist module, readings will be in the format:
+    # "|<beacon_ts>,<local_ts>,<accel_x>,<accel_y>,<accel_z>|"
+    # from base module, readings will be in the format:
+    # "|<beacon_ts>,<local_ts>,<load_cell>|"
+    # uid is a tuple (connected_ip_address, connected_socket)
+    # returns a tuple ("wrist" or "base", dictionary (created below))
+
+    base_data = False
+    dict = {"adc":[], "local_ts": [], "beacon_ts": []}
+    data = str(data)
+    data = data.replace("b'", "")
+    data = data.replace("'", "")
+    data = data.split("|")
+    try:
+        for reading in data:
+            reading = reading.split(",")
+            if len(reading) < 2:
+                continue
+            beacon_ts = reading[0]
+            local_ts = reading[1]
+            if int(beacon_ts) == 3200171710 or int(local_ts) == 3200171710:
+                continue
+            if len(reading) == 3:  # from the base module
+                load_cell = reading[3]
+                dict["beacon_ts"].append(beacon_ts)
+                dict["local_ts"].append(local_ts)
+                dict["adc"].append(load_cell)
+                base_data = True
+            elif len(reading) == 5: #from the wrist module
+                x = reading[3]
+                y = reading[4]
+                z = reading[5]
+                accel = math.sqrt(math.pow(x, 2) + math.pow(y, 2) + math.pow(z, 2))
+                dict["beacon_ts"].append(beacon_ts)
+                dict["local_ts"].append(local_ts)
+                dict["adc"].append(accel)
+    except Exception as e:
+        logger.info("Unexpected data format, exception:")
+        logger.info(e)
+
+    if len(dict["beacon_ts"]) == len(dict["local_ts"]) == len(dict["adc"]):
+        name = ""
+        if base_data:
+            name = "base"
+        else:
+            name = "wrist"
+        return name, dict
+    else:
+        print("Lengths of the arrays are unequal")
+        exit(-1)
+
+
+
+
+def parse_mcu_msg(data, uid):
+    # from wrist module, readings will be in the format:
+    # "|<beacon_ts>,<local_ts>,<accel_x>,<accel_y>,<accel_z>|"
+    # from base module, readings will be in the format:
+    # "|<beacon_ts>,<local_ts>,<load_cell>|"
+    # uid is a tuple (connected_ip_address, connected_socket)
+
+    ip_addr = uid[0]
+    data = str(data)
+    data = data.replace("b'", "")
+    data = data.replace("'", "")
+    data = data.split("|")
+    try:
+        for reading in data:
+            reading = reading.split(",")
+            if len(reading)<2:
+                continue
+            beacon_ts = reading[0]
+            local_ts = reading[1]
+            if int(beacon_ts) == 3200171710 or int (local_ts) == 3200171710:
+                continue
+            if len(reading) == 3:   # from the base module
+                load_cell = reading[3]
+            logger.info("Recieved from board {}:\n\tBeacon timestamp:\t{}\n\tLocal timestamp:\t{}".format(ip_addr, beacon_ts,
+                                                                                                    local_ts))
+            store_data(ip_addr, beacon_ts, local_ts)
+    except Exception as e:
+        logger.info("Unexpected data format, exception:")
+        logger.info(e)
+
+
+def store_data(uid, beacon_timestamp, local_time):
+    board_file = create_files(uid)
+    f = open(os.path.join(os.getcwd(), "timestamp_data", board_file), 'a')
+    f.write('\n' + str(beacon_timestamp) + ',' + str(local_time))
+    f.close()
+
+
+
+def run_experiment():
+    setup()
+    linux()
+
+
+def cleanup():
+    # deletes folder timestamp data and files inside it
+    folder = os.path.join(os.getcwd(), "timestamp_data")
+    files = os.listdir(folder)
+    for file in files:
+        filepath = os.path.join(folder, file)
+        os.remove(filepath)
+    os.remove(folder)
+
+if __name__ == '__main__':
+    run_experiment()
+    if SAVE:
+        cleanup()
--- board_communication/test.py
+++ board_communication/test.py
@@ -1,11 +1,5 @@
 import datetime
 import os
 
-a = "b'asfdasdfsad'"
-
-b = a.find("b'")
-print(b)
-c = a[b+2:]
-print(c)
-d = a.replace("b'", "")
-print("d: {}".format(d))
+if(3==3==3):
+    print("True")
