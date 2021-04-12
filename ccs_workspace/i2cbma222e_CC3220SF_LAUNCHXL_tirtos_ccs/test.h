1367fffbd89c0f1999728d06bf43e1b2183ef193
nnobinelo <nnobinelo98@live.com>
2021-04-11 22:42:23 -0400

    re-added all original ccs_workspace files

---
+++ ccs_workspace/i2cbma222e_CC3220SF_LAUNCHXL_tirtos_ccs/test.h
@@ -0,0 +1,19 @@
+/*
+ * test.h
+ *
+ *  Created on: Nov 12, 2020
+ *      Author: NNobi
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#include <stdint.h>
+#include <ti/sail/bma2x2/bma2x2.h>
+
+int32_t init_accel();
+
+int32_t get_accel();
+
+
+#endif /* TEST_H_ */
