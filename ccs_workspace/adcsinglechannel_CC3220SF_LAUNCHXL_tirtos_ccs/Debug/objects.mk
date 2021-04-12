df49d09fe17cf60bd5170716d1f1d1ced19705b5
nnobinelo <nnobinelo98@live.com>
2021-04-11 22:29:34 -0400

    removing ccs_workspace to re-add later

--- ccs_workspace/adcsinglechannel_CC3220SF_LAUNCHXL_tirtos_ccs/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lti_utils_build_linker.cmd.genlibs $(GEN_CMDS__FLAG) -l"ti/devices/cc32xx/driverlib/ccs/Release/driverlib.a" -llibc.a
-
