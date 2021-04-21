81f4dc8c8c530593c74262c2f1e29776a5216fa0
jonahobw <jgow98@gmail.com>
2021-04-21 10:27:23 -0400

    data for slomo and same time trigger

--- board_communication/reconstruct_experiments.py
+++ board_communication/reconstruct_experiments.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy
 
 
-def parse_same_time_experiment(experiment_folder):
+def parse_same_time_experiment(experiment_folder, plot = True):
     wrist_file = os.path.join(experiment_folder, "wrist_data.txt")
     base_file = os.path.join(experiment_folder, "base_data.txt")
 
@@ -27,24 +27,72 @@ def parse_same_time_experiment(experiment_folder):
     base_y = [float(x) for x in base_y]
     f.close()
 
+    wrist_derivative = numpy.gradient(wrist_y)
+    base_derivative = numpy.gradient(base_y)
+
+    if plot:
+        fig, ax = plt.subplots(2)
+        ax[0].plot(wrist_x, wrist_y, label="wrist")
+        ax[0].plot(base_x, base_y, label="base")
+
+        ax[0].set_xlabel("Beacon Timestamp in ms")
+        ax[0].set_ylabel("Relative ADC readings")
+        ax[0].legend()
+
+        ax[1].plot(wrist_x, wrist_derivative, label="wrist_derivative")
+        ax[1].plot(base_x, base_derivative, label="base_derivative")
+
+        ax[1].set_xlabel("Beacon Timestamp in ms")
+        ax[1].set_ylabel("Derivative of ADC Readings")
+        ax[1].legend()
+        plt.show()
+
+    return wrist_x, wrist_y, wrist_derivative, base_x, base_y, base_derivative
+
+
+def calculate_timestamps(wrist_x, wrist_der, base_x, base_der, threshold = 0.1):
+    # uses the derivative of base and wrist to timestamp data
+    # timestamp is first time derivative exceeds threshold
+
+    wrist_ts = None
+    base_ts = None
+
+    for i in range(len(wrist_der)):
+        if abs(wrist_der[i]) > threshold:
+            wrist_ts = wrist_x[i]
+            break
+
+    for i in range(len(base_der)):
+        if abs(base_der[i]) > threshold:
+            base_ts = base_x[i]
+            break
+    return wrist_ts, base_ts
+
+def plot_with_ts(wrst_x, wrst_y, wrst_der, wrst_ts, base_x, base_y, base_der, base_ts, folder):
     fig, ax = plt.subplots(2)
-    ax[0].plot(wrist_x, wrist_y, label="wrist")
+    ax[0].plot(wrst_x, wrst_y, label="wrist")
     ax[0].plot(base_x, base_y, label="base")
 
     ax[0].set_xlabel("Beacon Timestamp in ms")
     ax[0].set_ylabel("Relative ADC readings")
-    ax[0].legend()
 
-    wrist_derivative = numpy.gradient(wrist_y)
-    base_derivative = numpy.gradient(base_y)
-
-    ax[1].plot(wrist_x, wrist_derivative, label="wrist_derivative")
-    ax[1].plot(base_x, base_derivative, label="base_derivative")
+    ax[1].plot(wrst_x, wrst_der, label="wrist_derivative")
+    ax[1].plot(base_x, base_der, label="base_derivative")
 
     ax[1].set_xlabel("Beacon Timestamp in ms")
     ax[1].set_ylabel("Derivative of ADC Readings")
+    ax[0].vlines(wrst_ts, ymin=0, ymax= 1, linestyles='dashed', label='wrist_ts')
+    ax[0].vlines(base_ts, ymin=0, ymax=1, linestyles='dashed', label='base_ts')
+    ax[1].vlines(wrst_ts, ymin=0, ymax=1, linestyles='dashed', label='wrist_ts')
+    ax[1].vlines(base_ts, ymin=0, ymax=1, linestyles='dashed', label='base_ts')
     ax[1].legend()
+    ax[0].legend()
+    plt.suptitle("Wrist_ts: {}, Base_ts: {}, diff: {}\n{}".format(wrst_ts,
+                                                                  base_ts,
+                                                                  wrst_ts-base_ts,
+                                                                  folder))
     plt.show()
+
 
 def same_time_experiment_reconstruct(folder = "2021-04-15_23;29;40"):
     cwd = os.getcwd()
@@ -52,6 +100,47 @@ def same_time_experiment_reconstruct(folder = "2021-04-15_23;29;40"):
     experiment_folder = os.path.join(cwd, "same_time_trigger_data", folder)
     parse_same_time_experiment(experiment_folder)
 
+def get_all_timestamps(folder = "same_time", hist = True, plot_greater_than = None, hist_limit = None):
+    # folder is the root folder containing one folder for each experiment, each experiment folder contains
+    # a wrist_data.txt and base_data.txt file
+    # if plot_greater than is not none, it should be an int, and for each experiment where the difference is
+    # greater than <plot_greater_than> the experiment will be plotted
+    # if hist_limit is not none, then it should be an integer that will limit the x axis range for the histogram
+    if folder is "same_time":
+        folder = os.path.join(os.getcwd(), "same_time_trigger_data")
+    if folder is "slomo":
+        folder = os.path.join(os.getcwd(), "slo_mo_testing")
+
+    #print(len(os.listdir(folder)))
+
+    diff = []
+
+    for experiment in os.listdir(folder):
+        experiment_folder = os.path.join(folder, experiment)
+        wrst_x, wrst_y, wrst_der, base_x, base_y, base_der = parse_same_time_experiment(experiment_folder,
+                                                                                        plot=False)
+        wrist_ts, base_ts = calculate_timestamps(wrst_x, wrst_der,base_x, base_der)
+        exp_diff = wrist_ts - base_ts
+        if plot_greater_than is not None:
+            if abs(exp_diff) > plot_greater_than:
+                plot_with_ts(wrst_x, wrst_y, wrst_der, wrist_ts,
+                             base_x, base_y, base_der, base_ts, experiment_folder)
+        if hist_limit is not None:
+            if abs(exp_diff) < hist_limit:
+                diff.append(exp_diff)
+        else:
+            diff.append(exp_diff)
+        print(wrist_ts, base_ts, diff)
+
+    if hist:
+        plt.hist(diff, bins=30)
+        plt.title("Difference between wrist timestamp and base timestamp\n{} trials".format(len(diff)))
+        plt.xlabel("Drift in ms")
+        plt.ylabel("Frequency")
+        plt.show()
+
+
 if __name__ == '__main__':
-    filename = "2021-04-15_23;29;55"
-    same_time_experiment_reconstruct(filename)
+    # filename = "2021-04-15_23;29;55"
+    # same_time_experiment_reconstruct(filename)
+    get_all_timestamps(hist_limit=None, plot_greater_than=None)
--- flask_app/main_app.py
+++ flask_app/main_app.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import mpld3
 import numpy as np
 import board_communication.system_commands as sys_cmds
+from board_communication.reconstruct_experiments import calculate_timestamps
 
 views = Blueprint('auth', __name__)
 loading_plots = False
@@ -35,7 +36,7 @@ def get_modules_plot():
 
 
 @views.route('/load-modules-plot', methods=['POST'])
-def load_modules_plot():
+def load_modules_plot(save = False, relevant_data = False, vertical_lines = False):
     global loading_plots, modules_plot_html
 
     if loading_plots:
@@ -47,71 +48,105 @@ def load_modules_plot():
     if res is not None:
         wrist_x_axis, wrist_y, base_x_axis, base_y = res
 
-        # print(len(wrist_x_axis), len(wrist_y))
-        # print(len(base_x_axis), len(base_y))
+        wrist_1d = [x[0] for x in wrist_y.tolist()]
+        base_1d = [x[0] for x in base_y.tolist()]
+        print(len(wrist_1d))
+        print(len(base_1d))
+        wrist_derivative = numpy.gradient(wrist_1d)
+        base_derivative = numpy.gradient(base_1d)
+
+        wrist_ts, base_ts = calculate_timestamps(wrist_x_axis, wrist_derivative, base_x_axis, base_derivative)
+
+        if relevant_data:
+            wrist_index = wrist_x_axis.index(wrist_ts)
+            wrist_lower = wrist_index-10 if wrist_index-10>=0 else 0
+            wrist_higher = wrist_index+30 if wrist_index+30<len(wrist_x_axis) else len(wrist_x_axis)-1
+
+            base_index = base_x_axis.index(base_ts)
+            base_lower = base_index - 10 if base_index -10 >=0 else 0
+            base_higher = base_index + 30 if base_index + 30 <len(base_x_axis) else len(base_x_axis)
+
+            lower = min(wrist_lower, base_lower)
+            higher = max(wrist_higher, base_higher)
+            if higher>len(wrist_x_axis) or higher > len(base_x_axis):
+                higher = min(len(wrist_x_axis, len(base_x_axis)))
+
+            wrist_x_axis = wrist_x_axis[lower:higher]
+            wrist_y = wrist_y[lower:higher]
+            wrist_derivative = wrist_derivative[lower:higher]
+
+            base_x_axis = base_x_axis[lower:higher]
+            base_y = base_y[lower:higher]
+            base_derivative = base_derivative[lower:higher]
 
         fig, ax = plt.subplots(2)
         ax[0].plot(wrist_x_axis, wrist_y, label="wrist")
         ax[0].plot(base_x_axis, base_y, label="base")
+        if vertical_lines:
+            ax[0].vlines(wrist_ts, ymin=0, ymax=1, colors='g',
+                         linestyles='dashed', label='wrist_ts')
+            ax[0].vlines(base_ts, ymin=0, ymax=1, colors='r',
+                         linestyles='dashed', label='base_ts')
 
         ax[0].set_xlabel("Beacon Timestamp in ms")
         ax[0].set_ylabel("Relative ADC readings")
-        ax[0].legend()
-
-        wrist_1d = [x[0] for x in wrist_y.tolist()]
-        base_1d = [x[0] for x in base_y.tolist()]
-        wrist_derivative = numpy.gradient(wrist_1d)
-        base_derivative = numpy.gradient(base_1d)
+        ax[0].legend(loc = 'upper right')
 
         ax[1].plot(wrist_x_axis, wrist_derivative, label="wrist_derivative")
         ax[1].plot(base_x_axis, base_derivative, label="base_derivative")
+        if vertical_lines:
+            ax[1].vlines(wrist_ts, ymin=-0.5, ymax=0.5,colors='g',
+                         linestyles='dashed', label='wrist_ts')
+            ax[1].vlines(base_ts, ymin=-0.5, ymax=0.5, colors='r',
+                         linestyles='dashed', label='base_ts')
 
         ax[1].set_xlabel("Beacon Timestamp in ms")
         ax[1].set_ylabel("Derivative of ADC Readings")
-        ax[1].legend()
-        plt.show()
+        ax[1].legend(loc = 'upper right')
+        #plt.show()
 
-        # save fig to image to disk---------------------------------------------------
+        if save:
+            # save fig to image to disk---------------------------------------------------
 
-        # if root folder does not exist, save it
-        same_time_data_folder = os.path.join(os.getcwd(), "same_time_trigger_data")
-        if not os.path.exists(same_time_data_folder):
-            os.mkdir(same_time_data_folder)
+            # if root folder does not exist, save it
+            same_time_data_folder = os.path.join(os.getcwd(), "slo_mo_testing")
+            if not os.path.exists(same_time_data_folder):
+                os.mkdir(same_time_data_folder)
 
-        # save folder is in the format year month day hour min sec
-        a = str(datetime.datetime.now()).split(".")[0]
-        b = a.replace(" ", "_")
-        folder_name = b.replace(":", ";")
-        folder_path = os.path.join(same_time_data_folder, folder_name)
+            # save folder is in the format year month day hour min sec
+            a = str(datetime.datetime.now()).split(".")[0]
+            b = a.replace(" ", "_")
+            folder_name = b.replace(":", ";")
+            folder_path = os.path.join(same_time_data_folder, folder_name)
 
-        #make the folder
-        os.mkdir(folder_path)
-        fig.savefig(os.path.join(folder_path, "output.png"))
+            #make the folder
+            os.mkdir(folder_path)
+            fig.savefig(os.path.join(folder_path, "output.png"))
 
-        # save base module data
-        base_txt_file = os.path.join(folder_path, "base_data.txt")
-        f = open(base_txt_file, 'w')
-        base_x = str(base_x_axis[0])
-        for i in range(1, len(base_x_axis)):
-            base_x += "," + str(base_x_axis[i])
-        f.write(str(base_x))
-        f.write("\n")
-        base_y_arr = [x[0] for x in base_y.tolist()]
-        f.write(str(base_y_arr))
-        f.close()
+            # save base module data
+            base_txt_file = os.path.join(folder_path, "base_data.txt")
+            f = open(base_txt_file, 'w')
+            base_x = str(base_x_axis[0])
+            for i in range(1, len(base_x_axis)):
+                base_x += "," + str(base_x_axis[i])
+            f.write(str(base_x))
+            f.write("\n")
+            base_y_arr = [x[0] for x in base_y.tolist()]
+            f.write(str(base_y_arr))
+            f.close()
 
-        # save wrist module data
-        wrist_txt_file = os.path.join(folder_path, "wrist_data.txt")
-        f = open(wrist_txt_file, 'w')
-        wrist_x = str(wrist_x_axis[0])
-        for i in range(1, len(wrist_x_axis)):
-            wrist_x += "," + str(wrist_x_axis[i])
-        f.write(str(wrist_x))
-        f.write("\n")
-        wrist_y_arr = [x[0] for x in wrist_y.tolist()]
-        f.write(str(wrist_y_arr))
-        f.close()
-        #----------------------------------------------------------------
+            # save wrist module data
+            wrist_txt_file = os.path.join(folder_path, "wrist_data.txt")
+            f = open(wrist_txt_file, 'w')
+            wrist_x = str(wrist_x_axis[0])
+            for i in range(1, len(wrist_x_axis)):
+                wrist_x += "," + str(wrist_x_axis[i])
+            f.write(str(wrist_x))
+            f.write("\n")
+            wrist_y_arr = [x[0] for x in wrist_y.tolist()]
+            f.write(str(wrist_y_arr))
+            f.close()
+            #----------------------------------------------------------------
 
         modules_plot_html = mpld3.fig_to_html(fig)
 
